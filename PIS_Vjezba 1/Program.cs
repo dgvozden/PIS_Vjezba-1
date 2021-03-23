e7750c1283cfd685899c28ad627b420b55f54f93
DJurko <dgvozden@morh.hr>
2021-03-23 09:42:06 +0100

    Dodano nešto novo (Zadatak 16.a)

--- PIS_Vjezba 1/Program.cs
+++ PIS_Vjezba 1/Program.cs
@@ -7,6 +7,7 @@ namespace PIS_Vjezba_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Dodano nešto novo");
         }
     }
 }
